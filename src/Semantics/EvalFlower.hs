5516786e6df5e283d9891c47f1465319be8342a7
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2011-04-06 15:32:53 +0200

    INT: Computation monad renamed to Evaluation

--- src/Semantics/EvalFlower.hs
+++ src/Semantics/EvalFlower.hs
@@ -17,7 +17,7 @@ import Semantics.TypesFlower
 
 
 type Bindings = Map Ident Value
-type Computation a = StateT Bindings Err a
+type Evaluation a = StateT Bindings Err a
 
 eval = evalProgram
 
@@ -27,47 +27,47 @@ evalProgram program = evalStateT (programEvaluator program) baseBindings
 baseBindings :: Bindings
 baseBindings = empty
 
-programEvaluator :: Program -> Computation Program
+programEvaluator :: Program -> Evaluation Program
 programEvaluator (Program declarations) =
     liftM Program $ sequence $ do
         d <- declarations
         return $ abstractDeclarationEvaluator d
 
-abstractDeclarationEvaluator :: AbstractDeclaration -> Computation AbstractDeclaration
+abstractDeclarationEvaluator :: AbstractDeclaration -> Evaluation AbstractDeclaration
 abstractDeclarationEvaluator abstractDeclaration@(ADLet aDeclaration) = do
     declarationEvaluator aDeclaration
     return abstractDeclaration
 
-declarationEvaluator :: Declaration -> Computation Declaration
+declarationEvaluator :: Declaration -> Evaluation Declaration
 declarationEvaluator declaration@(DLet anIdentifier aType anExpression) = do
     check anExpression aType
     addValue anIdentifier anExpression
     return declaration
 
-check :: Expr -> Type -> Computation ()
+check :: Expr -> Type -> Evaluation ()
 check anExpr aType = do
     expected <- typeName aType
     actual <- deduceType anExpr
     if expected == actual then return () else typeError expected actual
 
-typeName :: Type -> Computation String
+typeName :: Type -> Evaluation String
 typeName (TypeId (Ident aName)) = return aName
 
-deduceType :: Expr -> Computation String
+deduceType :: Expr -> Evaluation String
 deduceType (ExprConst _) = return "Int"
 deduceType (ExprId identifier) = boundValueType identifier
 
-addValue :: Ident -> Expr -> Computation ()
+addValue :: Ident -> Expr -> Evaluation ()
 addValue identifier expression = do
     bindings <- get
     put $ insert identifier (IntV expression) bindings
 
-boundValueType :: Ident -> Computation String
+boundValueType :: Ident -> Evaluation String
 boundValueType identifier = do
     value <- getValue identifier
     deduceType value
 
-getValue :: Ident -> Computation Expr
+getValue :: Ident -> Evaluation Expr
 getValue anIdentifier@(Ident aName) = do
     bindings <- get
     maybe
@@ -78,11 +78,11 @@ getValue anIdentifier@(Ident aName) = do
 omitType :: Value -> Expr
 omitType (IntV expr) = expr
 
-typeError :: String -> String -> Computation a
+typeError :: String -> String -> Evaluation a
 typeError expectedType gotType =
     fail $ concat ["Type mismatch - expected: ", expectedType,
         " got: ", gotType, "!"]
 
-nameError :: String -> Computation a
+nameError :: String -> Evaluation a
 nameError aName =
     fail $ concat ["Could not find name \"", aName, "\"", "!"]
