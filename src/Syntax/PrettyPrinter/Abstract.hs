c5975dffe126c4bf65529f124ab044c124436ca8
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2011-04-27 17:26:48 +0200

    INT: Type pretty-printing

--- src/Syntax/PrettyPrinter/Abstract.hs
+++ src/Syntax/PrettyPrinter/Abstract.hs
@@ -6,6 +6,18 @@ import Syntax.Abstract
 
 
 instance Show Type where
-    show (TypeId ident) = ident
-    show (TypeFun left right) = concat [show left, " -> ", "(", show right, ")"]
-    show (TypeApp left right) = concat ["(", show left, ")", " ", show right]
+    show = showType 0
+
+showType :: Int -> Type -> String
+showType 0 (TypeFun t0 t1) = concat [showType 1 t0, arrow, showType 0 t1]
+showType 1 (TypeApp t0 t1) = concat [showType 1 t0, " ", showType 2 t0]
+showType 2 (TypeId i) = i
+showType 0 t = showType 1 t
+showType 1 t = showType 2 t
+showType 2 t = braces $ showType 0 t
+
+braces :: String -> String
+braces s = concat ["(", s, ")"]
+
+arrow :: String
+arrow = " -> "
