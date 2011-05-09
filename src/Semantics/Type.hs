ea56646fb8d621866354185ccc7dfbc54fa16278
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2011-05-09 16:49:30 +0200

    EVAL-4: Recursive functions typing

--- src/Semantics/Type.hs
+++ src/Semantics/Type.hs
@@ -25,24 +25,26 @@ checkProg (Prog declarations) = forM_ declarations checkDeclaration
 
 checkDeclaration :: Monad m => Decl -> TypeCheckT m ()
 checkDeclaration (Let aPoly anIdent aType anExpr) = do
-    check anExpr aType aPoly
+    check anIdent anExpr aType aPoly
     modify $ fix . (\baseBounds thisBounds ->
         insert anIdent ((Context aType aPoly thisBounds), anExpr) baseBounds)
 
-check :: Monad m => Expr -> Type -> [Poly] -> TypeCheckT m Type
-check anExpr decType aPoly = do
-    infType <- typeOf anExpr
+check :: Monad m => Ident -> Expr -> Type -> [Poly] -> TypeCheckT m Type
+check anIdent anExpr decType aPoly = do
+    infType <- typeOf anIdent decType aPoly anExpr
     infType <: (decType, aPoly)
     return decType
 
 (<:) :: Monad m => InferredType -> (DeclaredType, [Poly]) -> m ()
 infType <: (decType, aPoly) = runTeqT (match infType decType) aPoly []
 
-typeOf :: Monad m => Expr -> TypeCheckT m Type
-typeOf = infer >=> unify
+typeOf :: Monad m => Ident -> Type -> [Poly] -> Expr -> TypeCheckT m Type
+typeOf anIdent aType aPoly = infer anIdent aType aPoly >=> unify
 
-infer :: Monad m => Expr -> TypeCheckT m [Condition]
-infer anExpr = do { anEnv <- get ; runInferT inference anEnv anExpr }
+infer :: Monad m => Ident -> Type -> [Poly] -> Expr -> TypeCheckT m [Condition]
+infer anIdent aType aPoly anExpr = do
+    anEnv <- get
+    runInferT inference (insert anIdent (Context aType aPoly anEnv, ExprIdent anIdent) anEnv) anExpr
 
 unify :: Monad m => [Condition] -> TypeCheckT m Type
 unify = runUnifyT unification $ typeVar typeIndex0
